4c60ace489dd03ab0011f2c2b4a933a44b44a86e
anacielo17 <anacieloseidel@gmail.com>
2023-05-09 22:38:19 -0300

    cambios para el render

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require ("express")
 const path = require ("path")
 const app = express();
+const port = process.env.PORT || 3001;
+app.listen(port, () =>console.log("servidor corriendo en el puerto ${port}"));
 app.use (express.static("public"));
 
 app.get('/', (req, res)=>{
@@ -18,6 +20,3 @@ app.get('/login.html', (req, res)=>{
     res.sendFile (path.join(__dirname, './views/login.html'))
     
 })
-app.listen(3000, ()=>{
-    console.log("Servidor escuchando en el puerto 3000")
-});
